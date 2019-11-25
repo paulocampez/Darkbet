fb820e2d77786d2a32667c6b7e032b76a8fca806
METAORA\paulo.campez <prcampez@gmail.com>
2019-11-25 14:27:27 -0300

    Ajustado componente do angular

--- Darkbet/src/angular/darkbet/src/app/app.component.ts
+++ Darkbet/src/angular/darkbet/src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'darkbet';
+  colors = [
+    "red",
+    "black",
+    "red",
+    "black",
+    "red",
+    "black",
+    "red",
+    "black",
+    "red",
+    "black",
+    "red",
+    "black",
+  ];
+  //10, 9, 6 ,2
+  prizes = [
+    "Grand prize",
+    "demo 2",
+    "demo 3",
+    "demo 4",
+    "demo 5",
+    "demo 6",
+    "demo 7",
+    "demo 8",
+    "demo 9",
+    "demo 10",
+    "demo 11",
+    "demo 12"
+  ];
+  beforeSpinFn = function () {
+    // alert("before");
+  };
+  spinOnce = false;
+  afterSpinFn = function () {
+    // alert("after");
+  };
 }
+
