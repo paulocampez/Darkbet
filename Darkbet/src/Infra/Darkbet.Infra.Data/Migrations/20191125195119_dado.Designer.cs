d508b8cc77f285f15f4cd81050f6ae1e305572ed
METAORA\paulo.campez <prcampez@gmail.com>
2019-11-25 16:51:44 -0300

    adicionado migration

--- Darkbet/src/Infra/Darkbet.Infra.Data/Migrations/20191125195119_dado.Designer.cs
+++ Darkbet/src/Infra/Darkbet.Infra.Data/Migrations/20191125195119_dado.Designer.cs
@@ -4,14 +4,16 @@ using Darkbet.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Darkbet.Infra.Data.Migrations
 {
     [DbContext(typeof(DarkbetContext))]
-    partial class DarkbetContextModelSnapshot : ModelSnapshot
+    [Migration("20191125195119_dado")]
+    partial class dado
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Darkbet/src/Infra/Darkbet.Infra.Data/Migrations/20191125195119_dado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Darkbet.Infra.Data.Migrations
+{
+    public partial class dado : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "dices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Number = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dices", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "dices");
+        }
+    }
+}
